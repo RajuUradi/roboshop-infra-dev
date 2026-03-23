c0c403b6269764f7bfbda6e00b861694b2d12cde
RajuUradi <rajuuradi75@gmail.com>
2026-03-23 09:00:45 +0530

    made changes in 40-databases

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -30,6 +30,6 @@ data "aws_ssm_parameter" "rabbitmq_sg_id"{
    name="/${var.project}/${var.environment}/rabbitmq/sg_id"
 }
 
-data "aws_ssm_parameter" "mysql"{
+data "aws_ssm_parameter" "mysql_sg_id"{
     name="/${var.project}/${var.environment}/mysql/sg_id"
 }
