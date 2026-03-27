0d9621caeb52b7181fe67eddfced8cf25ee0291f
RajuUradi <rajuuradi75@gmail.com>
2026-03-27 13:29:19 +0530

    first commit

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,8 +1,8 @@
 locals{
     ami_id=data.aws_ami.devops.id
     catalogue_sg_id=data.aws_ssm_parameter.catalogue_sg_id.value
-    private_subnet_id=data.aws_ssm_parameter.private_subnet_id
-    vpc_id=data.aws_ssm_parameter.vpc_id
+    private_subnet_id=split("," , data.aws_ssm_parameter.private_subnet_id.value)[0]
+    vpc_id=data.aws_ssm_parameter.vpc_id.value
     backendalb_listener_arn=data.aws_ssm_parameter.backend-alb-listener-arn.value
     common_tags={
         Project ="Roboshop"
